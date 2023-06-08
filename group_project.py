64bd944a5775e2dca051b95cd3e74cba2bbd7732
dh8sn8 <diephangst@gmail.com>
2023-06-08 23:55:14 +0200

    Update repo

--- group_project.py
+++ group_project.py
@@ -1,7 +1,8 @@
-import pyomo.environ as pyo
+#import pyomo.environ as pyo
 import pandas as pd
-from pyomo.environ import ConcreteModel, Var, Objective, Constraint, SolverFactory
+#from pyomo.environ import ConcreteModel, Var, Objective, Constraint, SolverFactory
 
 demand = pd.read_csv('load.csv',header=None)
 demand.rename(columns={0:"timestep", 1:"load_MW"}, inplace=True)
-demand
+
+print(demand)
